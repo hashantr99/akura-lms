e23f990a01253dcd36351d4930338bc98a54b448
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2024-04-15 18:15:06 +0530

    Chapter Publish Action

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,11 +5,13 @@ import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
+import { Banner } from "@/components/banner";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideoForm } from "./_components/chapter-video-form";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
     params
@@ -46,8 +48,17 @@ const ChapterIdPage = async ({
     const completedFields = requiredFields.filter(Boolean).length;
 
     const completionText = `(${completedFields}/${totalFields})`;
+
+    const isComplete = requiredFields.every(Boolean);
     
     return ( 
+        <>
+        {!chapter.isPublished && (
+            <Banner 
+                variant="warning"
+                label="This chapter is unpublished. It will not be visible in the course"
+            />
+        )}
         <div className="p-6">
             <div className="flex items-center justify-between">
                 <div className="w-full">
@@ -66,6 +77,12 @@ const ChapterIdPage = async ({
                                 Complete all fields {completionText}
                             </span>
                         </div>
+                        <ChapterActions 
+                            disabled={!isComplete}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                            isPublished={chapter.isPublished}
+                        />
                     </div>
                 </div>
             </div>
@@ -118,6 +135,7 @@ const ChapterIdPage = async ({
                 </div>
             </div>
         </div>
+        </>
      );
 }
  
