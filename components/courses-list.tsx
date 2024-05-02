40fbc7289612be74e31c5cfa85bd2361d414b3f1
hashantr99 <123436883+hashantr99@users.noreply.github.com>
2024-05-03 02:19:44 +0530

    Course & Chapters Layout

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -19,15 +19,25 @@ export const CoursesList = ({
         <div>
             <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
                 {items.map((item) => (
+                    // <CourseCard 
+                    //     key={item.id}
+                    //     id={item.id}
+                    //     title={item.title}
+                    //     imageUrl={item.imageUrl!}
+                    //     chaptersLength={item.chapters.length}
+                    //     price={item.price!}
+                    //     progress={item.progress}
+                    //     category={item?.category?.name!}
+                    // />
                     <CourseCard 
                         key={item.id}
                         id={item.id}
                         title={item.title}
-                        imageUrl={item.imageUrl!}
+                        imageUrl=""
                         chaptersLength={item.chapters.length}
-                        price={item.price!}
+                        price={11}
                         progress={item.progress}
-                        category={item?.category?.name!}
+                        category="abc"
                     />
                 ))}
             </div>
